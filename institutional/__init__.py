bec7b65ab3e30d9ddf3353a51c44a6510bcacc50
oliveirasamuel03-jpg <oliveirasamuel03@gmail.com>
2026-04-19 03:33:27 -0300

    Create institutional dashboard package exports

---
+++ institutional/__init__.py
@@ -0,0 +1,6 @@
+from .dashboard import build_institutional_dashboard_payload, render_institutional_dashboard
+
+__all__ = [
+    "build_institutional_dashboard_payload",
+    "render_institutional_dashboard",
+]
